136ed71a3b6e30a630a466532833488c2c6db1dd
zeldacl <zeldacl@gmail.com>
2020-04-11 17:44:19 +0800

    temp save

--- src/main/clojure/cn/li/mcmod/client/cgui/core.clj
+++ src/main/clojure/cn/li/mcmod/client/cgui/core.clj
@@ -1,12 +1,24 @@
 (ns cn.li.mcmod.client.cgui.core)
 
+(defn breathe-alpha []
+  (let [time (System/currentTimeMillis)
+        sin (* (+ 1 (Math/sin (/ time 0.8))) 0.5)]
+    (+ 0.675 (* sin 0.175))))
+
 (defmacro defcomponent [name fields]
-  (let [fields (conj fields 'widget)]
+  (let [fields (conj fields 'widget 'name)]
     `(defrecord ~name ~fields)))
+
+
+(defcomponent ComponentTransform [pos size])
+
 
 ;(defrecord Component [])
 
-(defrecord Widget [])
+(defmacro defwidget [name fields]
+  (let [fields (conj fields 'transform 'name)]
+    `(defrecord ~name ~fields)))
+;(defrecord Widget [])
 
 (defprotocol Render
   (render [this]))
