8ca1c3e7086afb2f4dc92fb9d3f7c517477d2044
zeldacl <zeldacl@gmail.com>
2025-03-19 10:40:43 +0800

    temp save

--- forge_1_16_5/src/main/clojure/cn/academy/block/block_registry.clj
+++
@@ -1,7 +0,0 @@
-(ns cn.academy.block.block-registry
-  (:require [cn.academy.block.registration-impl :as reg-impl]
-            [cn.academy.block.registry-def :as registry]))
-
-(defn register-blocks! [mod-id]
-  (let [registration (reg-impl/create-registration)]
-    (registry/register-all! registration mod-id)))
