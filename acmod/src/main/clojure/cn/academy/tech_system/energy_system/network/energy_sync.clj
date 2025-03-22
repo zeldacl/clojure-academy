518c78ec4fbdc9deb9bdc435b0f58936c8dba10a
zeldacl <zeldacl@gmail.com>
2025-03-22 13:20:22 +0800

    temp save

---
+++ acmod/src/main/clojure/cn/academy/tech_system/energy_system/network/energy_sync.clj
@@ -0,0 +1,35 @@
+(ns cn.academy.tech-system.energy-system.network.energy-sync
+  (:require [mcmod.network :refer [IPacket]]
+            [mcmod.capabilities :as cap]
+            [mcmod.world :as world]))
+
+(defrecord EnergySyncPacket [pos energy-stored]
+  IPacket
+  (encode [this buf]
+    (doto buf
+      (.writeInt (:x pos))
+      (.writeInt (:y pos))
+      (.writeInt (:z pos))
+      (.writeInt energy-stored)))
+  
+  (decode [this buf]
+    (->EnergySyncPacket
+     {:x (.readInt buf)
+      :y (.readInt buf)
+      :z (.readInt buf)}
+     (.readInt buf)))
+  
+  (handle [this ctx]
+    (let [world (world/get-world ctx)
+          block-pos {:x (:x pos), :y (:y pos), :z (:z pos)}
+          tile-entity (world/get-tile-entity world block-pos)]
+      (when tile-entity
+        ;; Update client-side energy storage
+        (when-let [energy-storage (cap/get-energy-capability tile-entity nil)]
+          (cap/deserialize-nbt energy-storage {"energy" energy-stored}))))))
+
+(defn create-energy-sync-packet [pos energy-stored]
+  (->EnergySyncPacket pos energy-stored))
+
+(defn register-packet! [channel]
+  (channel/register-message! channel "energy_sync" ->EnergySyncPacket))
